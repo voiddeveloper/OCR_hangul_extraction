bd0da130beb30fe7224a6683be38fd96c16c950d
Od-team <gkdl1206@gmail.com>
2019-12-19 22:14:39 +0900

    이미지끼리 비교하기 작업중

--- hwang/comparisonImage/imageDetectAndComparison.py
+++ hwang/comparisonImage/imageDetectAndComparison.py
@@ -5,12 +5,30 @@ import math
 ###############################################################
 # 전역 변수 모음
 # 테스트할 파일 이름
-standardFile = 'img/test_standard3.png'
-comparisonFile = 'img/test_comparison3.png'
+standardFile = 'hwang/imgSet/test_standard2.png'
+comparisonFile = 'hwang/imgSet/test_comparison2.png'
 # 비교 픽셀 오차 범위
-# errorRangePixel = 5
-errorRangePixel = 0.1
+errorRangePixel = 5
+# errorRangePixel = 0.05
+# 비교하지 않는 픽셀 범위
+exceptionPixel = 8
+# 이미지를 분할하여 구분할 때 분할 범위
+divisionCount = 4
 ###############################################################
+
+# 목표: 기준 이미지와 비슷한 모양을 비교 이미지에서 찾아낸다.
+# 기준 이미지와 종횡비가 맞다면 크기가 달라져도 찾아내야 한다. 종횡비가 다르다면 걸러내야 한다.
+# 방법
+# 1. 기준 이미지에서 contour 영역을 찾아낸 후, 서로간 상호 거리를 구한다.
+# 2. 비교 이미지에서 contour 영역을 찾아낸다.
+# 3. 비교 이미지에서 각 contour와 기준 이미지의 contour 크기 비율을 비교한다. 비율이 똑같지 않은 것은 버린다.
+# (완전 똑같을 수는 없기 때문에 오차 범위를 준다. errorRangePixel)
+# 너무 작은 값은 오차율이 심해서 비교하지 않는다. exceptionPixel)
+# 4. 같은 비율의 contour를 찾아냈다면, 해당 contour 주변에 기준 이미지처럼 상호 거리도 같은지 비교한다. 거리도 비율로 구하고, 똑같지 않은 것은 버린다.
+# (아까와 마찬가지로 완전 똑같을 수는 없기 때문에 오차 범위를 준다. errorRangePixel)
+# 5. 상호 거리가 일치하고, 상호 연결된 contours 갯수도 같다면 같은 이미지로 본다.
+##################################################################################################
+# 개선 사항: 여기서 추가적으로 기준 이미지의 contour와 비교 이미지의 contour 픽셀을 비교하면 좀 더 정확할 것 같다.
 
 # 해당 위치에 label을 입력하는 메서드
 def setLabel(image, str, contour):
@@ -22,8 +40,8 @@ def setLabel(image, str, contour):
     cv.putText(image, str, (ptX, ptY), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 1, 8)
 
 # 기준 이미지, 비교 이미지 설정
-imgStandard = cv.imread(standardFile)
-imgComparison = cv.imread(comparisonFile)
+imgStandard = cv.imread(standardFile, cv.IMREAD_COLOR)
+imgComparison = cv.imread(comparisonFile, cv.IMREAD_COLOR)
 
 ########################################################################################
 # 1. 기준 이미지에서 어떤 영역들이 얼마나 떨어진 위치에 있는지 값을 구한다.
@@ -33,10 +51,6 @@ imgGrayscale = cv.cvtColor(imgStandard, cv.COLOR_BGR2GRAY)
 
 # 그레이스케일로 변환된 이미지를 바이너리로 변환
 ret, imgBinary = cv.threshold(imgGrayscale, 127, 255, cv.THRESH_BINARY_INV|cv.THRESH_OTSU)
-# cv.imshow('original', imgStandard)
-# cv.imshow('grayscale', imgGrayscale)
-# cv.imshow('binary', imgBinary)
-# cv.waitKey(0)
 
 # contours = 동일한 색을 가지고 있는 영역의 경계선 정보
 # RETR_EXTERNAL = contours 정보 중에서 바깥쪽 라인만 찾는다.
@@ -61,9 +75,9 @@ for i in range(len(contours)):
     # 테투리로 영역 설정하기
     # cv.drawContours(imgStandard, [cnt], 0, (0, 255, 0), 2)
     # 사각형으로 영역 설정하기 
-    cv.rectangle(imgStandard, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    # cv.rectangle(imgStandard, (x, y), (x + w, y + h), (0, 255, 0), 1)
     rectangleCount += 1
-    setLabel(imgStandard, str(rectangleCount), cnt)
+    # setLabel(imgStandard, str(rectangleCount), cnt)
 
 # 영역별 거리값 구하기
 for i in range(len(boxPoint)):
@@ -125,69 +139,90 @@ for i in range(len(boxPointComparison)):
 
 # boxPoint = 기준 이미지의 Contour (x, y, w, h) 좌표
 # contoursDistance = 기준 이미지에서 1번째 Contour를 기준으로 다른 Contour들 간 얼마나 떨어져 있는지 비교한다. (x축 거리, y축 거리)
-
 # boxPointComparison = 비교 이미지의 Contour (x, y, w, h) 좌표
 # contoursDistanceComparison = 비교 이미지에서 1번째 Contour를 기준으로 다른 Contour들 간 얼마나 떨어져 있는지 비교한다. (x축 거리, y축 거리)
-
 print('boxPoint = ', boxPoint)
 print('contoursDistance = ', contoursDistance)
 print('boxPointComparison = ', boxPointComparison)
 print('contoursDistanceComparison = ', contoursDistanceComparison)
 print('boxPoint[0][2] = ', boxPoint[0][2])
 
+# for i in range(len(boxPoint)):
+#     imgCut = imgBinary.copy()
+#     imgCut = imgBinary[boxPoint[i][1]:boxPoint[i][1] + boxPoint[i][3], boxPoint[i][0]:boxPoint[i][0] + boxPoint[i][2]]
+#     imgResize = cv.resize(imgCut, (0, 0), fx = divisionCount, fy = divisionCount, interpolation= cv.INTER_AREA)
+#     demensions = imgResize.shape
+#     print(demensions)
+#     # cv.imshow('de', imgResize)
+#     # cv.waitKey(0)
+#     # cv.destroyWindow('de')
+#     for j in range(divisionCount):
+#         for k in range(divisionCount):
+#             contourCut = imgResize.copy()
+#             contourCut = imgResize[k * boxPoint[i][3]:k * boxPoint[i][3] + boxPoint[i][3], j * boxPoint[i][2]:j * boxPoint[i][2] + boxPoint[i][2]]
+#             # cv.imshow('de', contourCut)
+#             # cv.waitKey(0)
+#             # cv.destroyWindow('de')
+
+
+########################################################################################
+# 3. 기준 이미지와 비교 이미지의 비율 차이를 구하고, 비율이 동일한지 확인한다.
+########################################################################################
 for i in range(len(boxPointComparison)):
     # 기준 영역과 비교 영역 간 비율 차이 (width 값으로 구하기)
     ratio =  boxPointComparison[i][2] / boxPoint[0][2]
-    print('i = ', i, 'ratio = ', ratio)
-    # 늘어난 비율만큼을 적용해서 height값도 일치하는지 확인 (+- 3픽셀의 오차 범위를 준다.)
+    
+    # 너무 작은 영역은 비교하지 않는다.
+    # 늘어난 비율만큼을 적용해서 height값도 일치하는지 확인 (+- 오차 범위를 준다.)
     # 이 조건을 일치하지 않으면 어차피 같은 비율의 그림이 아니기 때문에 더 이상 비교할 필요가 없다.
-    # if(boxPointComparison[i][3] <= (boxPoint[0][3] * ratio) + errorRangePixel and boxPointComparison[i][3] >= (boxPoint[0][3] * ratio) - errorRangePixel):
-    if(boxPointComparison[i][3] <= (boxPoint[0][3] * ratio) * (1 + errorRangePixel) and boxPointComparison[i][3] >= (boxPoint[0][3] * ratio) * (1 - errorRangePixel)):
-        flag = []
-        # 조건이 일치됐으면, 같은 비율만큼 떨어진 거리에 기준 이미지와 똑같은 영역 분포가 있는지 확인한다.
-        for j in range(len(contoursDistance)):
-            print ('i =', i, 'j =', j)
-            # 다음 영역이 있어야 하는 예상 위치
-            expectX = boxPointComparison[i][0] + (contoursDistance[j][0] * ratio)
-            expectY = boxPointComparison[i][1] + (contoursDistance[j][1] * ratio)
-            print('ratio = ', ratio, 'expectX = ', expectX, 'expectY = ', expectY)
+    if boxPointComparison[i][2] > exceptionPixel and boxPointComparison[i][3] > exceptionPixel:
+        if(boxPointComparison[i][3] <= (boxPoint[0][3] * ratio) + errorRangePixel and boxPointComparison[i][3] >= (boxPoint[0][3] * ratio) - errorRangePixel):
+        # if(boxPointComparison[i][3] <= (boxPoint[0][3] * ratio) * (1 + errorRangePixel) and boxPointComparison[i][3] >= (boxPoint[0][3] * ratio) * (1 - errorRangePixel)):
             
-            # 정말로 예상 위치에 다음 영역이 존재하는지 확인한다.
-            for k in range(len(boxPointComparison)):
-                
-                # 예상 위치에 정말 영역이 존재한다면 몇번째 영역인지 저장한다.
-                # if(boxPointComparison[k][0] >= expectX - errorRangePixel and boxPointComparison[k][0] <= expectX + errorRangePixel and boxPointComparison[k][1] >= expectY - errorRangePixel and boxPointComparison[k][1] <= expectY + errorRangePixel):
-                if(boxPointComparison[k][0] >= expectX * (1 - errorRangePixel) and boxPointComparison[k][0] <= expectX * (1 + errorRangePixel) and boxPointComparison[k][1] >= expectY * (1 - errorRangePixel) and boxPointComparison[k][1] <= expectY * (1 + errorRangePixel)):
-                    print('k = ', k)
-                    flag.append(k)
+            ########################################################################################
+            # 4. 비율이 동일하다면, 기준 이미지와 비교 이미지의 거리 차이를 구하고, 거리가 동일한지 확인한다. (위에서 구한 비율로 거리값을 조절한다.)
+            ########################################################################################
+            flag = []
+            for j in range(len(contoursDistance)):
+                # 다음 영역이 있어야 하는 예상 위치
+                expectX = boxPointComparison[i][0] + (contoursDistance[j][0] * ratio)
+                expectY = boxPointComparison[i][1] + (contoursDistance[j][1] * ratio)            
+                # 정말로 예상 위치에 다음 영역이 존재하는지 확인한다.
+                for k in range(len(boxPointComparison)):
+                    # 예상 위치에 정말 영역이 존재한다면 몇번째 영역인지 저장한다.
+                    if(boxPointComparison[k][0] >= expectX - errorRangePixel and boxPointComparison[k][0] <= expectX + errorRangePixel and boxPointComparison[k][1] >= expectY - errorRangePixel and boxPointComparison[k][1] <= expectY + errorRangePixel):
+                    # if(boxPointComparison[k][0] >= expectX * (1 - errorRangePixel) and boxPointComparison[k][0] <= expectX * (1 + errorRangePixel) and boxPointComparison[k][1] >= expectY * (1 - errorRangePixel) and boxPointComparison[k][1] <= expectY * (1 + errorRangePixel)):
+                        flag.append(k)
 
-        finalOrder = []            
-        # 저장된 모든 값의 갯수와 기준 영역의 갯수가 같다면, 기준 이미지와 동일한 이미지이다.
-        # 해당 영역을 차지하는 좌표의 min, max값을 구해서 테두리를 친다.
-        if len(contoursDistance) == len(flag):
-            finalOrder.append(i)
-            for l in range(len(flag)):
-                finalOrder.append(flag[l])
+            ########################################################################################
+            # 5. 이 모든 조건을 전부 충족하면 기준 이미지와 유사한 이미지다.
+            # 해당 영역을 차지하는 좌표의 min, max값을 구해서 테두리를 친다.
+            ########################################################################################
+            finalOrder = []
+            if len(contoursDistance) == len(flag):
+                finalOrder.append(i)
+                for l in range(len(flag)):
+                    finalOrder.append(flag[l])
 
-            minX = boxPointComparison[finalOrder[0]][0]
-            maxX = boxPointComparison[finalOrder[0]][0] + boxPointComparison[finalOrder[0]][2]
-            minY = boxPointComparison[finalOrder[0]][1]
-            maxY = boxPointComparison[finalOrder[0]][1] + boxPointComparison[finalOrder[0]][3]
+                minX = boxPointComparison[finalOrder[0]][0]
+                maxX = boxPointComparison[finalOrder[0]][0] + boxPointComparison[finalOrder[0]][2]
+                minY = boxPointComparison[finalOrder[0]][1]
+                maxY = boxPointComparison[finalOrder[0]][1] + boxPointComparison[finalOrder[0]][3]
 
-            for m in range(len(finalOrder)):
-                if minX > boxPointComparison[finalOrder[m]][0]:
-                    minX = boxPointComparison[finalOrder[m]][0]
+                for m in range(len(finalOrder)):
+                    if minX > boxPointComparison[finalOrder[m]][0]:
+                        minX = boxPointComparison[finalOrder[m]][0]
 
-                if maxX < boxPointComparison[finalOrder[m]][0] + boxPointComparison[finalOrder[m]][2]:
-                    maxX = boxPointComparison[finalOrder[m]][0] + boxPointComparison[finalOrder[m]][2]
-                
-                if minY > boxPointComparison[finalOrder[m]][1]:
-                    minY = boxPointComparison[finalOrder[m]][1]
+                    if maxX < boxPointComparison[finalOrder[m]][0] + boxPointComparison[finalOrder[m]][2]:
+                        maxX = boxPointComparison[finalOrder[m]][0] + boxPointComparison[finalOrder[m]][2]
+                    
+                    if minY > boxPointComparison[finalOrder[m]][1]:
+                        minY = boxPointComparison[finalOrder[m]][1]
 
-                if maxY < boxPointComparison[finalOrder[m]][1] + boxPointComparison[finalOrder[m]][3]:
-                    maxY = boxPointComparison[finalOrder[m]][1] + boxPointComparison[finalOrder[m]][3]
+                    if maxY < boxPointComparison[finalOrder[m]][1] + boxPointComparison[finalOrder[m]][3]:
+                        maxY = boxPointComparison[finalOrder[m]][1] + boxPointComparison[finalOrder[m]][3]
 
-            cv.rectangle(imgComparison, (minX, minY), (maxX, maxY), (0, 0, 255), 2)
+                cv.rectangle(imgComparison, (minX, minY), (maxX, maxY), (0, 0, 255), 2)
 
 cv.imshow('comparison', imgComparison)
 cv.waitKey(0)
