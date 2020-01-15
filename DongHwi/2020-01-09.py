3c78e5be6ad347e4c7b5989de9e614a447e1f566
Od-team <56684517+Od-team@users.noreply.github.com>
2020-01-15 21:58:39 +0900

    색상필터를 거쳐 나온 이미지에서 컨투어를 뽑고, 3개필터 적용한 해서 네모영역만드는 코드

--- DongHwi/2020-01-09.py
+++ DongHwi/2020-01-09.py
@@ -31,9 +31,10 @@ def colorFilter(img_color, color_dict):
         upper1 = np.array(color_dict['upper_range'])
         mask = cv2.inRange(image, lower1, upper1)
         result = cv2.bitwise_and(result, result, mask=mask)
-    # if color == 'green':
-    #     cv2.imshow(str(color_dict['color'])+str(color_dict['s']) + str(' , ')+str(color_dict['v']), mask)
-    #     cv2.waitKey(0)
+
+    # if color == 'black':
+    # cv2.imshow(str(color_dict['color'])+str(color_dict['s']) + str(' , ')+str(color_dict['v']), mask)
+    # cv2.waitKey(0)
 
     return mask
 
@@ -48,10 +49,21 @@ def findContour(image):
 
 
 # 컨투어 x,y 최소, 최대 값 찾기
-def findMinMaxPoint(contour):
+def findMinMaxPoint(bi_image, image, contour):
     min_max_list = []
 
+    remove_flag = False
+
+    h, w, c = image.shape
+
     for i, con in enumerate(contour):
+        remove_flag = False
+
+        # 글자는 최소 4개의 꼭지점으로 이루어져있다.
+        # 따라서 3개 이하면 글자가 아니다
+        if len(con) <= 3:
+            continue
+
         x_min = 9999
         x_max = 0
         y_min = 9999
@@ -69,56 +81,70 @@ def findMinMaxPoint(contour):
 
             if y_max < j[0][1]:
                 y_max = j[0][1]
-        min_max_list.append([x_min, y_min, x_max, y_max])
+
+        """
+        1번 필터
+        가로 세로 길이가 모두 10픽셀 이하인 글자는 잡지 않겠다.
+        """
+        if (x_max - x_min) <= 10 and (y_max - y_min) <= 10:
+            continue
+        else:
+
+            """
+            2번 필터
+            네모영역안의 픽셀을 검사해서 15~50% 미만이면 글자로 판단 그리고 95%이상이면 글자로 판단( ㅡ, ㅣ ), 그렇지 않으면 잡음으로 처리한다.
+            * 글자 ( 자음, 모음 ) 영역을 그렸을때 픽셀양이 45% 이하인걸 확인.
+            """
+            white_sum = 0
+
+            for height in range(y_min, y_max):
+                for width in range(x_min, x_max):
+                    if bi_image[height][width] == 255:
+                        white_sum += 1
+
+            pixel_sum = ((white_sum) / ((x_max - x_min) * (y_max - y_min)))
+
+            # 픽셀양이 10~50% 이하이면 네모영역 그림
+            if (pixel_sum * 100 <= 55 and pixel_sum * 100 >= 10) or pixel_sum * 100 > 95:
+
+                """
+                3번 필터 
+                정사각형 제외 - 맑은고딕은 네모꼴임 그래서 자음 모음이 정사각형 모양이 나올 수 없음
+                """
+                if (x_max - x_min) / (y_max - y_min) == 1.0:
+                    continue
+                else:
+                    op_draw = cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 0, 255), 1)
+                    cv2.imshow('sdf', op_draw)
+
+                    # 모든 필터에 통과한다면 min_max_list에 네모영역 좌표를 추가한다.
+                    min_max_list.append([x_min, y_min, x_max, y_max])
 
     return min_max_list
-
-
-def minMaxRemoveFilter(image, min_max_point_list):
-    height, width, channel = image.shape
-
-    print(height, width, channel)
-
-    """ 1. 이미지의 가로, 세로크기의 절반 영역을 잡은 경우는 제외한다."""
-    if height > width:
-        max_limit = int((height * width) / 2)
-    else:
-        max_limit = int((height * width) / 2)
-
-    new_min_max_point_list = []
-
-    for i in min_max_point_list:
-        if (i[2] - i[0]) * (i[3] - i[1]) < max_limit:
-            new_min_max_point_list.append(i)
-            # draw = cv2.rectangle(img_color, (i[0], i[1]), (i[2], i[3]), (0, 0, 255), 2)
-    # cv2.imshow('asd',draw)
-    # cv2.waitKey(0)
-
-    return new_min_max_point_list
-
 
 if __name__ == '__main__':
 
     start_time = time.time()
 
     # rgb 이미지 불러오기
-    img_color = cv2.imread('qq5.png')
+    img_color = cv2.imread('comparison_image_12.png')
 
-    # img_color = cv2.resize(img_color, dsize=(1500, 1500), interpolation=cv2.INTER_AREA)
+    # cv2.imshow('image', img_color)
+    # cv2.waitKey(0)
+
+    h, w, c = img_color.shape
+
+    print(h, w, c)
 
     # rgb -> hsv 변환
     img_hsv = cv2.cvtColor(img_color, cv2.COLOR_RGB2HSV)
     img_yuv = cv2.cvtColor(img_color, cv2.COLOR_RGB2YUV)
-    # cv2.imshow('sdf12e',img_color)
-    # cv2.imshow('sdf12e',img_yuv)
-    # cv2.imshow('sdf',img_hsv)
-    # cv2.waitKey(0)
 
     # 색 필터 적용후 이미지 리스트
     result_filter_list = []
 
-    s_range = 12
-    v_range = 12
+    s_range = 10
+    v_range = 10
 
     """ 검은색 """
     color_dict = {}
@@ -139,7 +165,6 @@ if __name__ == '__main__':
     color_dict['v'] = 0
     white_filter_result_image = colorFilter(img_color, color_dict)
     result_filter_list.append(white_filter_result_image)
-
 
     for s in range(s_range):
         for v in range(v_range):
@@ -224,56 +249,31 @@ if __name__ == '__main__':
             color_dict['v'] = v * (256 / v_range)
             purple_filter_result_image = colorFilter(img_color, color_dict)
             result_filter_list.append(purple_filter_result_image)
-            #
-            # """ 검은색 """
-            # color_dict = {}
-            # color_dict['color'] = 'black'
-            # color_dict['lower_range'] = [0, 0, 0]
-            # color_dict['upper_range'] = [180, 0, 0]
-            # black_filter_result_image = colorFilter(img_color, color_dict)
-            # result_filter_list.append(black_filter_result_image)
-            #
-            # """ 하얀색 """
-            # color_dict = {}
-            # color_dict['color'] = 'white'
-            # color_dict['lower_range'] = [0, 0, 255]
-            # color_dict['upper_range'] = [180, 255, 255]
-            # white_filter_result_image = colorFilter(img_color, color_dict)
-            # result_filter_list.append(white_filter_result_image)
 
     """ 
     이진화 처리된 이미지에서 컨투어를 찾는다.
     컨투어를 찾고 주변 다른 컨투어와 묶었을때 정사각형과 비슷하게 나오면 글자로 추정한다. 
     """
 
-    # # 색 필터 이미지에서 컨투어를 찾고
-    # # x,y 최소 / x,y 최대 값을 찾아서 새로운 new_min_max_point_list 를 만든다.
-    # min_max_point_list = []
-    # new_min_max_point_list = []
-    #
-    # for image in result_filter_list:
-    #
-    #     contour = findContour(image)
-    #     point_list = findMinMaxPoint(contour)
-    #
-    #     for point in point_list:
-    #         min_max_point_list.append(point)
-    #
-    # new_min_max_point_list = minMaxRemoveFilter(img_color, min_max_point_list)
+    # 색 필터 이미지에서 컨투어를 찾고
+    # x,y 최소 / x,y 최대 값을 찾아서 새로운 new_min_max_point_list 를 만든다.
+    min_max_point_list = []
+    new_min_max_point_list = []
 
-    # ddddd = cv2.imread('comparison_image_14.png')
-    # ddddd = cv2.cvtColor(ddddd, cv2.COLOR_RGB2HSV)
-    #
-    # ddddd = cv2.resize(ddddd, dsize=(300, 300), interpolation=cv2.INTER_AREA)
-    #
-    # h,w,c = ddddd.shape
-    #
-    # cv2.imshow('ads',ddddd)
-    # cv2.waitKey(0)
-    #
-    # for i in range(h):
-    #     for j in range(w):
-    #         print("",ddddd[i][j], end=" ")
-    #     print('\n')
+    su = 0
+
+    for bi_image in result_filter_list:
+
+        contour = findContour(bi_image)
+        point_list = findMinMaxPoint(bi_image, img_color, contour)
+
+        su += len(point_list)
+
+        for point in point_list:
+            min_max_point_list.append(point)
+
+
+    print('네모영역 개수 : ', su)
 
     print('time : ', time.time() - start_time)
+    cv2.waitKey(0)
