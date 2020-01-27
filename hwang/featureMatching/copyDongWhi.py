91d7f26de88a6f5fc7370151d02597b5a82c80ef
Od-team <gkdl1206@gmail.com>
2020-01-27 18:51:26 +0900

    자식 contour의 색깔 칠하는 알고리즘 수정 // 황준환

--- hwang/featureMatching/copyDongWhi.py
+++ hwang/featureMatching/copyDongWhi.py
@@ -45,11 +45,11 @@ def findContour(image):
     contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # 컨투어 반환
-    return contours
+    return contours,hierarchy
 
 
 # 컨투어 x,y 최소, 최대 값 찾기
-def findMinMaxPoint(bi_image, image, contour):
+def findMinMaxPoint(bi_image, image, contour, hierarchy, filter_variable):
     min_max_list = []
 
     remove_flag = False
@@ -86,39 +86,86 @@ def findMinMaxPoint(bi_image, image, contour):
         1번 필터
         가로 세로 길이가 모두 10픽셀 이하인 글자는 잡지 않겠다.
         """
-        if (x_max - x_min) <= 10 and (y_max - y_min) <= 10:
+        if (x_max - x_min) <= filter_variable['width_limit_pixel'] and (y_max - y_min) <= filter_variable['height_limit_pixel']:
             continue
         else:
 
-            """
-            2번 필터
-            네모영역안의 픽셀을 검사해서 15~50% 미만이면 글자로 판단 그리고 95%이상이면 글자로 판단( ㅡ, ㅣ ), 그렇지 않으면 잡음으로 처리한다.
-            * 글자 ( 자음, 모음 ) 영역을 그렸을때 픽셀양이 45% 이하인걸 확인.
-            """
-            white_sum = 0
+            # """
+            # 2번 필터
+            # 네모영역안의 픽셀을 검사해서 15~50% 미만이면 글자로 판단 그리고 95%이상이면 글자로 판단( ㅡ, ㅣ ), 그렇지 않으면 잡음으로 처리한다.
+            # * 글자 ( 자음, 모음 ) 영역을 그렸을때 픽셀양이 45% 이하인걸 확인.
+            # """
+            # white_sum = 0
+            #
+            # for height in range(y_min, y_max):
+            #     for width in range(x_min, x_max):
+            #         if bi_image[height][width] == 255:
+            #             white_sum += 1
+            #
+            # pixel_sum = ((white_sum) / ((x_max - x_min) * (y_max - y_min)))
+            #
+            # # 픽셀양이 10~50% 이하이면 네모영역 그림
+            # if (pixel_sum * 100 <= 55 and pixel_sum * 100 >= 10) or pixel_sum * 100 > 95:
+            #
+            # """
+            # 3번 필터
+            # 정사각형 제외 - 맑은고딕은 네모꼴임 그래서 자음 모음이 정사각형 모양이 나올 수 없음
+            # """
+            # if (x_max - x_min) / (y_max - y_min) == 1.0:
+            #     continue
 
-            for height in range(y_min, y_max):
-                for width in range(x_min, x_max):
-                    if bi_image[height][width] == 255:
-                        white_sum += 1
+            #픽셀의 색을 넣을 리스트
+            ll=[]
+            # 이미지와 동일한 크기의 검은색 이미지를 만든다
+            cimg=np.zeros_like(image)
+            # 검은색 이미지에 컨투어 크기만큼 흰색으로 그림
+            # 컨투어 안에도 흰색으로 차있음
+            cv2.drawContours(cimg,contour,i,color=255,thickness=-1)
+            #hierarchy에 해당 컨투어의 인덱스를 포함하는것 모두 검색
+            index,position=np.where(hierarchy[0]==i)
+            for n in range(len(index)):
+                #해당컨투어를 부모로 가진것만 검색해 자식 컨투어의 영역은 검은색으로 색칠
+                if position[n]==3:
+                    #####################################################################################
+                    # 자식 contour의 무게 중심을 구하고, 자식 컨투어의 상/하/좌/우를 한칸씩 땡기기 // 황준환
+                    # M = 자식 contour의 moments 값
+                    M = cv2.moments(contour[index[n]])
+                    # cx, cy = 자식 contour의 무게중심 좌표
+                    if M['m00'] != 0:
+                        cx = int(M['m10']/M['m00'])
+                        cy = int(M['m01']/M['m00'])
+                        # print (cx, cy)
+                        # print("계산전", contour[index[n]])
 
-            pixel_sum = ((white_sum) / ((x_max - x_min) * (y_max - y_min)))
+                        for cnt in contour[index[n]]:
+                            if cnt[0][0] < cx:
+                                cnt[0][0] = cnt[0][0] + 1
+                            elif cnt[0][0] > cx:
+                                cnt[0][0] = cnt[0][0] - 1
 
-            # 픽셀양이 10~50% 이하이면 네모영역 그림
-            if (pixel_sum * 100 <= 55 and pixel_sum * 100 >= 10) or pixel_sum * 100 > 95:
+                            if cnt[0][1] < cy:
+                                cnt[0][1] = cnt[0][1] + 1
+                            elif cnt[0][1] > cy:
+                                cnt[0][1] = cnt[0][1] - 1
 
-                """
-                3번 필터 
-                정사각형 제외 - 맑은고딕은 네모꼴임 그래서 자음 모음이 정사각형 모양이 나올 수 없음
-                """
-                if (x_max - x_min) / (y_max - y_min) == 1.0:
-                    continue
-                else:
-                    op_draw = cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 0, 255), 1)
-                    cv2.imshow('sdf', op_draw)
+                        # print("계산후", contour[index[n]])
+                    #####################################################################################
+                    cv2.drawContours(cimg, contour, index[n], color=0, thickness=-1)
+                    cv2.imshow('test', cimg)
+                    cv2.waitKey(0)
+            #흰색 좌표를 저장함
+            pts=np.where(cimg==255)
+            # 원본 이미지에서 해당 좌표에 어떤 색이 채워져 있는지 저장
+            ll.append(image[pts[0],pts[1]])
+            #중복을 제거해주는 부분
+            pixel_change_count = list(set([tuple(set(ll[0])) for ll[0] in ll[0]]))
+            if len(pixel_change_count) <= filter_variable['pixel_change_count']:
+                # op_draw = cv2.drawContours(image ,contour,i,color=(0,255,0),thickness=1)
+                op_draw = cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 0, 255), 1)
+                cv2.imshow('sdf', op_draw)
 
-                    # 모든 필터에 통과한다면 min_max_list에 네모영역 좌표를 추가한다.
-                    min_max_list.append([x_min, y_min, x_max, y_max])
+                # 모든 필터에 통과한다면 min_max_list에 네모영역 좌표를 추가한다.
+                min_max_list.append([x_min, y_min, x_max, y_max])
 
     return min_max_list
 
@@ -126,8 +173,30 @@ if __name__ == '__main__':
 
     start_time = time.time()
 
+    """
+       # h, v
+       s_range : s 범위 / int
+       v_range : v 범위 / int 
+       
+       # 컨투어 안에서 색상이 바뀌는 횟수 / int
+       filter_variable['pixel_change_count']
+       
+       # 가로픽셀이 10 이하인건 제외
+       filter_variable['width_limit_pixel']
+    
+       # 세로픽셀이 10 이하인건 제외
+       filter_variable['height_limit_pixel']
+       """
+
+    filter_variable = {}
+    s_range = 20
+    v_range = 20
+    filter_variable['width_limit_pixel'] = 10
+    filter_variable['height_limit_pixel'] = 10
+    filter_variable['pixel_change_count'] = 30
+
     # rgb 이미지 불러오기
-    img_color = cv2.imread('hwang/imgSet/test1_1.png')
+    img_color = cv2.imread('hwang/imgSet/test1_2.png')
 
     # cv2.imshow('image', img_color)
     # cv2.waitKey(0)
@@ -143,52 +212,37 @@ if __name__ == '__main__':
     # 색 필터 적용후 이미지 리스트
     result_filter_list = []
 
-    s_range = 10
-    v_range = 10
-
     """ 검은색 """
     color_dict = {}
     color_dict['color'] = 'black'
     color_dict['lower_range'] = [0, 0, 0]
-    color_dict['upper_range'] = [180, 0, 0]
+    color_dict['upper_range'] = [179, 255, 50]
     color_dict['s'] = 0
     color_dict['v'] = 0
-    black_filter_result_image = colorFilter(img_color, color_dict)
-    #################################################################
-    # 이 영역만 테스트로 수정중
-    contour = findContour(black_filter_result_image)
-    cv2.imshow('test', black_filter_result_image)
-    imgCopy = black_filter_result_image.copy()
-    imgCopyColor = cv2.cvtColor(imgCopy, cv2.COLOR_GRAY2BGR)
-
-    for cnt in contour:
-        # cv2.drawContours(imgCopyColor, [cnt], 0, (255, 0, 0), 1)
-        epsilon = 0.008 * cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, epsilon, True)
-        print("approx : ", len(approx))
-
-        # if len(approx) >= 4 and len(approx) <= 12:
-        cv2.drawContours(imgCopyColor, [approx], 0, (0, 0, 255), 1)
-
-    cv2.imshow('test2', imgCopyColor)
-    cv2.waitKey(0)
-
-
-    #################################################################
+    black_filter_result_image = colorFilter(img_hsv, color_dict)
     result_filter_list.append(black_filter_result_image)
 
     """ 하얀색 """
     color_dict = {}
     color_dict['color'] = 'white'
     color_dict['lower_range'] = [0, 0, 180]
-    color_dict['upper_range'] = [180, 50, 255]
+    color_dict['upper_range'] = [179, 50, 255]
     color_dict['s'] = 0
     color_dict['v'] = 0
-    white_filter_result_image = colorFilter(img_color, color_dict)
+    white_filter_result_image = colorFilter(img_hsv, color_dict)
     result_filter_list.append(white_filter_result_image)
 
     for s in range(s_range):
         for v in range(v_range):
+
+            # 하얀색 필터가 겹치면 제외
+            if s * (256 / s_range) < 50 and v * (256 / v_range) > 180:
+                continue
+
+            # 검은색 필터가 겹치면 제외
+            if s * (256 / s_range) < 255 and v * (256 / v_range) < 50:
+                continue
+
             # 색상 필터 적용해서 이미지에서 원하는 색을 뽑기
             """ 빨간색 """
             color_dict = {}
@@ -202,7 +256,7 @@ if __name__ == '__main__':
             color_dict['s'] = s * (256 / s_range)
             color_dict['v'] = v * (256 / v_range)
 
-            red_filter_result_image = colorFilter(img_color, color_dict)
+            red_filter_result_image = colorFilter(img_hsv, color_dict)
             result_filter_list.append(red_filter_result_image)
 
             """ 주황색 """
@@ -213,7 +267,7 @@ if __name__ == '__main__':
                                          v * (256 / v_range) + (256 / v_range)]
             color_dict['s'] = s * (256 / s_range)
             color_dict['v'] = v * (256 / v_range)
-            orange_filter_result_image = colorFilter(img_color, color_dict)
+            orange_filter_result_image = colorFilter(img_hsv, color_dict)
             result_filter_list.append(orange_filter_result_image)
 
             """ 노란색 """
@@ -224,7 +278,7 @@ if __name__ == '__main__':
                                          v * (256 / v_range) + (256 / v_range)]
             color_dict['s'] = s * (256 / s_range)
             color_dict['v'] = v * (256 / v_range)
-            yellow_filter_result_image = colorFilter(img_color, color_dict)
+            yellow_filter_result_image = colorFilter(img_hsv, color_dict)
             result_filter_list.append(yellow_filter_result_image)
 
             """ 초록색 """
@@ -235,7 +289,7 @@ if __name__ == '__main__':
                                          v * (256 / v_range) + (256 / v_range)]
             color_dict['s'] = s * (256 / s_range)
             color_dict['v'] = v * (256 / v_range)
-            green_filter_result_image = colorFilter(img_color, color_dict)
+            green_filter_result_image = colorFilter(img_hsv, color_dict)
             result_filter_list.append(green_filter_result_image)
 
             """ 연초록하늘색 """
@@ -246,7 +300,7 @@ if __name__ == '__main__':
                                          v * (256 / v_range) + (256 / v_range)]
             color_dict['s'] = s * (256 / s_range)
             color_dict['v'] = v * (256 / v_range)
-            skyblue_filter_result_image = colorFilter(img_color, color_dict)
+            skyblue_filter_result_image = colorFilter(img_hsv, color_dict)
             result_filter_list.append(skyblue_filter_result_image)
 
             """ 파란색 """
@@ -257,7 +311,7 @@ if __name__ == '__main__':
                                          v * (256 / v_range) + (256 / v_range)]
             color_dict['s'] = s * (256 / s_range)
             color_dict['v'] = v * (256 / v_range)
-            blue_filter_result_image = colorFilter(img_color, color_dict)
+            blue_filter_result_image = colorFilter(img_hsv, color_dict)
             result_filter_list.append(blue_filter_result_image)
 
             """ 보라색 """
@@ -268,7 +322,7 @@ if __name__ == '__main__':
                                          v * (256 / v_range) + (256 / v_range)]
             color_dict['s'] = s * (256 / s_range)
             color_dict['v'] = v * (256 / v_range)
-            purple_filter_result_image = colorFilter(img_color, color_dict)
+            purple_filter_result_image = colorFilter(img_hsv, color_dict)
             result_filter_list.append(purple_filter_result_image)
 
     """ 
@@ -285,8 +339,8 @@ if __name__ == '__main__':
 
     for bi_image in result_filter_list:
 
-        contour = findContour(bi_image)
-        point_list = findMinMaxPoint(bi_image, img_color, contour)
+        contour,hierarchy = findContour(bi_image)
+        point_list = findMinMaxPoint(bi_image, img_color, contour, hierarchy, filter_variable)
 
         su += len(point_list)
 
