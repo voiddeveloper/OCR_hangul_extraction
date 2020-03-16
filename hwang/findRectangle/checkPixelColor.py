484e76a0b20871062a63640f1a010d8061ac3812
Od-team <gkdl1206@gmail.com>
2020-03-16 19:10:52 +0900

    준환) 특정 좌표의 색상값 확인 코드

--- hwang/findRectangle/checkPixelColor.py
+++ hwang/findRectangle/checkPixelColor.py
@@ -5,20 +5,21 @@ import numpy as np
 # 특정 좌표의 색상값을 확인 해보기
 ###############################################
 
-# 이미지 불러오기
-imgColor = cv.imread('hwang/imgSet/findRectangle/real_4.jpg', cv.IMREAD_COLOR)
+# img: 이미지 파일
+# x: 색상값 확인할 x 좌표
+# y: 색상값 확인할 y 좌표
+def checkPixelColor(img, x, y):
+    # 이미지의 높이, 너비
+    height, width = img.shape[:2]
 
-# 불러온 이미지의 높이, 너비
-height, width = imgColor.shape[:2]
-# print(f'height={height}', f'width={width}')
+    # 픽셀값을 저장할 배열 생성
+    pixelInfo = [[0 for x in range(width)] for y in range(height)]
 
-# 픽셀값을 저장할 배열 생성
-pixelInfo = [[0 for x in range(width)] for y in range(height)]
+    # 픽셀값을 pixelInfo에 저장
+    for i in range(0, height):
+        for j in range(0, width):
+            pixelInfo[i][j] = img[i, j]
+    
+    # 원하는 좌표의 pixelInfo에 넣으면 해당 좌표의 색상값이 나온다.    
+    return pixelInfo[x][y]
 
-# 픽셀값을 pixelInfo에 저장
-for i in range(0, height):
-    for j in range(0, width):
-        pixelInfo[i][j] = imgColor[i, j]
-
-# 원하는 좌표의 pixelInfo에 넣으면 해당 좌표의 색상값이 나온다.
-# print(pixelInfo[63][302])
