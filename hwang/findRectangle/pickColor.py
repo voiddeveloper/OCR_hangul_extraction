73a96caecc04643621f3b28a43da305c378512a7
Od-team <gkdl1206@gmail.com>
2020-03-16 19:59:10 +0900

    준환) 색깔을 기준으로 네모 찾기 알고리즘

--- hwang/findRectangle/pickColor.py
+++ hwang/findRectangle/pickColor.py
@@ -1,21 +1,20 @@
 import math
 import cv2 as cv
 import numpy as np
-import time
 
 # 목표 : 화면에서 네모를 찾아라.
 # 1. 이미지를 읽는다.
 # 2. 읽어온 이미지를 HSV로 분류하고, 전체 픽셀 정보를 저장한다.
-# 3. 기준 색상을 정하고, 기준색상과 다른 픽셀 값을 찾기 시작한다.
+# 3. 기준 색상을 정하고, 기준색상과 다른 픽셀 값을 찾기 시작한다. (기준 색상은 (0,0) 좌표의 색상)
 # 4. 기준 색상과 다른 픽셀 값을 찾으면 해당 픽셀과 유사한 색상 범위를 가지는 mask를 만들고, 이 mask가 적용된 이미지를 뽑는다.
 # 5. mask가 적용된 이미지를 grayscale -> binary 처리한다. 이제 해당 색상만 보이는 이미지가 나온다.
 # 6. 이미지 값이 존재하는 폐곡선 영역을 찾는다. 선이 너무 세밀하면 안되기 때문에 모폴로지를 적용해서 적당히 선을 뭉갠다. 
-# 7. 해당 폐곡선의 꼭지점 갯수를 구한다. 꼭지점의 갯수가 4개인 것이 네모이다. 
+# 7. 해당 폐곡선의 꼭지점 갯수를 구한다. 꼭지점의 갯수가 4개인 것이 네모이다.
 
 ###############################################################
 # 전역 변수 모음
 # 테스트할 파일 이름
-testFile = 'hwang/imgSet/real_4.jpg'
+testFile = 'hwang/imgSet/findRectangle/real_2.jpg'
 # ROI 가로, 세로 길이 (ROI 내의 평균 색상을 구하는데 사용한다. 크기가 클수록 정확한 색상을 찾기 힘들다. 대신 계산량이 줄어든다.)
 roiLength = 5
 # mask 생성 시 H값의 범위 
@@ -23,16 +22,17 @@ maskDistance = 20
 # 모폴로지 적용할 kernel 크기 (커널 사이즈가 작을수록 인근한 색상을 하나의 집합으로 묶는다.)
 kernelSize = 11
 # 외곽선을 단순화하기 위한 epsilon 계수 (적을수록 세밀하게, 클수록 뭉뚝하게 외곽선을 그리게 된다.)
-epsilonCount = 0.015
+epsilonCount = 0.01
 # 색상의 유사도 거리 (거리가 가까울수록 인접한 색상이다.)
 ranges = 20
 # 찾은 사각형 카운트 수
 rectangleCount = 0
 ###############################################################
 
-# 2개의 색상값을 입력 받을 시, 2개의 색상 유사도 거리값 구하는 메소드
+# hsv Color 2개의 색상값을 입력 받을 시, 2개의 색상 유사도 거리값 구하는 메소드
+# hsvColor1 = 비교할 첫번째 색상
+# hsvColor2 = 비교할 두번째 색상
 def findTwoColorDistanceHSV(hsvColor1, hsvColor2):
-    # d = sqrt{(h1-h2)^2 + (s1-s2)^2 + (v1-v2)^2}
     hsvColor1H = int(hsvColor1[0])
     hsvColor1S = int(hsvColor1[1])
     hsvColor1V = int(hsvColor1[2])
@@ -40,18 +40,24 @@ def findTwoColorDistanceHSV(hsvColor1, hsvColor2):
     hsvColor2S = int(hsvColor2[1])
     hsvColor2V = int(hsvColor2[2])
 
+    # 1번째 색(hsv)의 공간 좌표 값
     x1 = (hsvColor1S * math.cos(2 * (math.pi * (hsvColor1H / 255))) * hsvColor1V) / 255
     y1 = (hsvColor1S * math.sin(2 * (math.pi * (hsvColor1H / 255))) * hsvColor1V) / 255
     z1 = hsvColor1V
     
+    # 2번째 색(hsv)의 공간 좌표 값
     x2 = (hsvColor2S * math.cos(2 * (math.pi * (hsvColor2H / 255))) * hsvColor2V) / 255
     y2 = (hsvColor2S * math.sin(2 * (math.pi * (hsvColor2H / 255))) * hsvColor2V) / 255
     z2 = hsvColor2V
 
+    # 유클리드 거리 계산법으로 두 점의 거리값을 구함
+    # distance = sqrt{(h1-h2)^2 + (s1-s2)^2 + (v1-v2)^2}
     distance = math.sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2) + pow((z1 - z2), 2)) 
     return distance
 
 # 비교대상 hsv 색상을 뽑는 메소드, ROI 크기를 설정하고 해당 크기 내의 평균값을 뽑는다.
+# x: ROI 가로 크기
+# y: ROI 세로 크기
 def makeROI(x, y):
     roi = imgColorBGR[y:y + roiLength, x: x + roiLength]
     roi = cv.medianBlur(roi, 3)
@@ -63,14 +69,20 @@ def makeROI(x, y):
     h = int(h)
     s = int(s)
     v = int(v)
-    
-    # print (h, s, v)
 
     return h,s,v
 
-# 입력 받은 색상을 기준으로 mask 생성하는 메소드
+# 입력 받은 색상(hsv)을 기준으로 mask 생성하는 메소드
+# h: 생성할 마스크의 h값
+# s: 생성할 마스크의 s값
+# v: 생성할 마스크의 v값
 def makeMaskImg(h, s, v):
     # 선택된 색상을 기준으로 비슷한 색상 lower, upper 범위 설정
+    # maskDistance = mask 생성 시 H값의 범위 (전역변수 선언하는 부분 참조)
+    # 3종류의 mask를 생성한다. (선택한 기준색상, 기준색상 + maskDistance, 기준색상 - maskDistance)
+    # 3종류의 mask 영역을 합쳐서 하나의 mask로 만든다.
+
+    # h값은 0과 180이 붙어있기 때문에, 예외처리를 한다.
     if h < maskDistance:
         lowerBlueA1 = np.array([h - maskDistance + 180, s, v])
         upperBlueA1 = np.array([180, 255, 255])
@@ -99,6 +111,8 @@ def makeMaskImg(h, s, v):
     imgMaskA1 = cv.inRange(imgColorHsv, lowerBlueA1, upperBlueA1)
     imgMaskA2 = cv.inRange(imgColorHsv, lowerBlueA2, upperBlueA2)
     imgMaskA3 = cv.inRange(imgColorHsv, lowerBlueA3, upperBlueA3)
+
+    # 3개의 마스크 영역을 합친다.
     temp = cv.bitwise_or(imgMaskA1, imgMaskA2)
     imgMaskResult = cv.bitwise_or(imgMaskA3, temp)
 
@@ -115,11 +129,7 @@ def makeMaskImg(h, s, v):
 # 입력받은 HSV 이미지를 이진화하기
 def changeBinaryImage(imageColor):
     imgGrayscale = cv.cvtColor(imageColor, cv.COLOR_BGR2GRAY)
-    ret, imgBinary = cv.threshold(imgGrayscale, 127, 255, cv.THRESH_BINARY|cv.THRESH_OTSU)
-    # cv.imshow('color', imageColor)
-    # cv.imshow('gray', imgGrayscale)
-    # cv.imshow('binary', imgBinary)
-    # cv.waitKey(0)
+    ret, imgBinary = cv.threshold(imgGrayscale, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
     
     return imgBinary
 
@@ -139,7 +149,7 @@ def findContoursInfo(originalImage, imageColor, imageBinary):
 
         # 반환된 도형의 꼭지점 갯수
         size = len(approx)
-        # 네모는 꼭지점이 4개다. 4개일때만 체크한다.
+        # 네모는 꼭지점이 4개다. 따라서, 꼭지점 값이 4개인 것만 체크한다.
         if cv.isContourConvex(approx):
             if size == 4:
                 cv.line(originalImage, tuple(approx[0][0]), tuple(approx[size-1][0]), (0, 255, 0), 3)
@@ -156,6 +166,7 @@ def findContoursInfo(originalImage, imageColor, imageBinary):
     # cv.waitKey(0)
     return originalImage
 
+# label 붙이기 메소드
 def setLabel(image, str, contour):
     (textWidth, textHeight), baseLine = cv.getTextSize(str, cv.FONT_HERSHEY_SIMPLEX, 0.7, 1)
     x, y, width, height = cv.boundingRect(contour)
@@ -164,16 +175,15 @@ def setLabel(image, str, contour):
     cv.rectangle(image, (ptX, ptY + baseLine), (ptX + textWidth, ptY - textHeight), (200, 200, 200), cv.FILLED)
     cv.putText(image, str, (ptX, ptY), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 1, 8)
 
-start_vect=time.time()
+## main 시작 ##
 
-# 1. 이미지를 BGR로 읽어오기
+# 1. 이미지 읽기
 imgColorBGR = cv.imread(testFile, cv.IMREAD_COLOR)
 copyImage = cv.imread(testFile, cv.IMREAD_COLOR)
-# 2. 읽어온 이미지를 BGR -> HSV로 변경
 imgColorHsv = cv.cvtColor(imgColorBGR, cv.COLOR_BGR2HSV)
+
 # 가로, 세로 길이 구하기
 height, width = imgColorBGR.shape[:2]
-# print(f'height={height}', f'width={width}')
 
 # 2. HSV 모든 픽셀 정보를 저장
 pixelInfo = [[0 for x in range(width)] for y in range(height)]
@@ -181,30 +191,28 @@ for i in range(0, height):
     for j in range(0, width):
         pixelInfo[i][j] = imgColorHsv[i, j]
 
-# 3. 기준 색상 정하기 (좌측 상단 시작점)
+# 3. 기준 색상 정하기 ((0, 0) 좌표)
 baseColor = pixelInfo[0][0]
 
-distanceArray = []
 savePixelList = []
 savePixelList.append(baseColor)
-
-# h = baseColor[0]
-# s = baseColor[1]
-# v = baseColor[2]
-# print(f'h={h}', f's={s}', f'v={v}')
-
 j = 0
 
 # 3. 기준 색상과 다른 픽셀을 찾는다.
 while j < height:
     i = 0
     while i < width:
-        # 비교군 색상을 정하고, 기준 색상과 비교군 색상의 색상 유사도 거리값을 구한다.
-        comparisonColor = pixelInfo[j][i]        
+
         arraySize = len(savePixelList)
         
         # 저장된 픽셀 정보와 현재 비교할 색상의 유사도 값을 전부 비교한다.
-        # 모든 비교값이 ranges보다 커야 한다. 하나라도 작다면 이미 사전에 비교한 색상이다.
+        # savePixelList에서 저장한 모든 값과 coparisonColor의 색상 거리값을 구한다.
+        # 색상 거리값이 ranges보다 커야 한다. 하나라도 작다면 이미 사전에 비교한 색상이다.
+        # comparisonColor : 비교할 색상값
+        # ranges: 두 색이 유사하다는 기준 값 (전역변수 선언하는 부분 참조)
+
+        comparisonColor = pixelInfo[j][i]
+    
         flag = True
         for k in range(0, arraySize):
             distance = findTwoColorDistanceHSV(savePixelList[k], comparisonColor)
@@ -212,7 +220,6 @@ while j < height:
                 flag = False
 
         # 위에서 비교한 결과 모든 비교값이 ranges보다 크다면 (flag == true라면)
-        # 만약 유사도 거리가 큰 색상을 찾았다면, 마스크를 생성한다.
         if flag == True:
             # 일단 이 색상은 비교할 색상으로 인정되었기 때문에, 픽셀 정보 배열에 저장한다.
             savePixelList.append(comparisonColor)
@@ -220,13 +227,14 @@ while j < height:
             roiColor = makeROI(i, j)
             # 생성된 ROI를 기반으로 하는 mask 생성
             resultImage = makeMaskImg(roiColor[0], roiColor[1], roiColor[2])
+            # 해당 mask가 적용된 binary 이미지 생성
             resultImageBinary = changeBinaryImage(resultImage)
+
+            # binary 이미지를 이용해서 네모가 있는지 판별
             findContoursInfo(copyImage, resultImage, resultImageBinary)
-        i += 5
-    j += 5
+        i += 1
+    j += 1
             
-print(savePixelList)
+# print(savePixelList)
 cv.imshow('result', copyImage)
-print("training Runtime: %0.2f Minutes"%((time.time() - start_vect)/60))
-# cv.imwrite('img/result_2.jpg', copyImage)
 cv.waitKey(0)
