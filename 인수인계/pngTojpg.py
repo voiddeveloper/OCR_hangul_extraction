5b445a21a84fbe8ecd6f721c8feefefa5c181e3e
Od-team <56684517+Od-team@users.noreply.github.com>
2020-03-19 21:20:28 +0900

    동휘 ) README.md 필독
    동휘 ) README.md 필독

--- 인수인계/pngTojpg.py
+++ 인수인계/pngTojpg.py
@@ -1,9 +1,15 @@
-""" 폴더안에 있는 이미지의 확장자를 jpg로 바꾸는 코드"""
+""" 
+폴더안에 있는 이미지의 확장자를 jpg로 바꾸는 코드
 
-folder_path = 'C:/ttf_result/malgun/'
+ex ) 폴더안에 image1.png , image2.png 파일이 있다면 image1.jpg , image2.jpg로 바꾸는 코드임
+"""
 
 from os import rename, listdir
-for i in listdir('C:/ttf_result/malgun/'):
+
+# 폰트파일이 있는 폴더의 경로
+folder_path = 'C:/ttf_result/malgun/'
+
+for i in listdir(folder_path):
     new_file = i.split(".")[0]
     rename(folder_path + i, folder_path + new_file + ".jpg")
   
